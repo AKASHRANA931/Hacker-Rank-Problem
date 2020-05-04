1f7c9e8666cab72c961c6abe88836f83d94da0c7
AKASHRANA931 <56543037+AKASHRANA931@users.noreply.github.com>
2020-05-04 23:13:31 +0530

    Create chek Weird or not.py

---
+++ chek Weird or not.py
@@ -0,0 +1,11 @@
+#Akash Rana
+n = int(input())
+if(n%2!=0):
+    print("Weird")
+elif(n%2==0 and n>=1 and n<=20):
+    print("Weird")
+elif(n%2==0 and n>=2 and n<=5):
+    
+    print("Not weird")
+elif(n%2 == 0 and n>20):
+    print("Not weird")
