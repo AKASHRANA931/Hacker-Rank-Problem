28a236415b8a335d35f4828408dad7eb5c3e3ca5
AKASHRANA931 <56543037+AKASHRANA931@users.noreply.github.com>
2020-05-05 09:06:14 +0530

    Update chek Weird or not.py

--- chek Weird or not.py
+++ chek Weird or not.py
@@ -1,11 +1,11 @@
 #Akash Rana
-n = int(input())
-if(n%2!=0):
-    print("Weird")
-elif(n%2==0 and n>=1 and n<=20):
-    print("Weird")
-elif(n%2==0 and n>=2 and n<=5):
-    
-    print("Not weird")
-elif(n%2 == 0 and n>20):
-    print("Not weird")
+if __name__ == '__main__':
+    n = int(input())
+    if( n%2 != 0):
+        print("Weird")
+    elif(n%2 == 0 and n in range(2,6)):
+        print("Not Weird")
+    elif(n%2 == 0 and n in range(6,21)):
+        print("Weird")
+    elif(n % 2 == 0 and n>20):
+        print("Not Weird")
